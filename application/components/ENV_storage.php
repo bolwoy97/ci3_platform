5f7b61205c6e751c0d963cbd4c4737e267bfa25d
bolwoy97 <bolwoy97@gmail.com>
2020-09-29 12:44:13 +0300

    add project

--- application/components/ENV_storage.php
+++ application/components/ENV_storage.php
@@ -14,8 +14,8 @@ $env_s = array();
 
 /**GENERAL ENV */
 
-$env_s['mail_adr'] = 'gridgroup.official@gmail.com';
-$env_s['mail_psw'] = 'O13081992o';
+$env_s['mail_adr'] = 'mail_adr';
+$env_s['mail_psw'] = 'mail_psw';
 
 $env_s['min_ord_buy_usd'] = 1;
 
@@ -25,21 +25,21 @@ $env_s['order_close_fee'] = 10;
 
 $env_s['ref_rate'] = 1;
 
-$env_s['api_secure_code'] = 'k92TyWe9uwgh493GY9384y2H';
+$env_s['api_secure_code'] = 'api_secure_code';
 
 $env_s['pm_form']['action'] = 'https://perfectmoney.com/api/step1.asp';
-$env_s['pm_form']['PAYEE_ACCOUNT'] = 'U9538241';
-$env_s['pm_form']['PAYEE_NAME'] = 'Grid Yard';
+$env_s['pm_form']['PAYEE_ACCOUNT'] = 'PAYEE_ACCOUNT';
+$env_s['pm_form']['PAYEE_NAME'] = 'PAYEE_NAME';
 $env_s['pm_form']['PAYMENT_UNITS'] = 'USD';
 
 /**coinpayments */
-$env_s['cp_pub_key'] = 'e65921b577287997dd507de4648544dbcdda10402d6febb9858791700752d41e';
-$env_s['cp_pri_key'] = 'd286ff0d7B8d6e9126A03B224A8AF63dF58862959D3c4f414C7812768B94477D';
-$env_s['cp_merchant_id'] = '11f0a06bde3b2cb311e26e9ced693836';
-$env_s['cp_secret'] = 'LGj26cCAz6ZeK5EsG1fM';
+$env_s['cp_pub_key'] = 'cp_pub_key';
+$env_s['cp_pri_key'] = 'cp_pri_key';
+$env_s['cp_merchant_id'] = 'cp_merchant_id';
+$env_s['cp_secret'] = 'cp_secret';
 
 /**perfectmoney */
-$env_s['pm_secret'] = '0ee7V40c5vWhpHZtFxKmFFecc';
+$env_s['pm_secret'] = 'pm_secret';
 
 /**LOCAL ENV */
 
@@ -57,17 +57,17 @@ $env_s['local']['grid_site'] = 'http://gridgroup/';
 
 /**SERVER ENV */
 
-$env_s['serv']['db']['hostname'] = '185.177.93.132';
-$env_s['serv']['db']['username'] = 'suser';
-$env_s['serv']['db']['password'] = 'dThN565c5H9LxdZR';
-$env_s['serv']['db']['database'] = 'grid_yard_ci';
+$env_s['serv']['db']['hostname'] = 'hostname.hostname.hostname.hostname';
+$env_s['serv']['db']['username'] = 'username';
+$env_s['serv']['db']['password'] = 'password';
+$env_s['serv']['db']['database'] = 'database';
 
-$env_s['serv']['db2']['hostname'] = '185.177.92.112';
-$env_s['serv']['db2']['username'] = 'suser';
-$env_s['serv']['db2']['password'] = 'dThN565c5H9LxdZR';
-$env_s['serv']['db2']['database'] = 'grid';
+$env_s['serv']['db2']['hostname'] = 'hostname.hostname.hostname.hostname';
+$env_s['serv']['db2']['username'] = 'username';
+$env_s['serv']['db2']['password'] = 'password';
+$env_s['serv']['db2']['database'] = 'database';
 
-$env_s['serv']['grid_site'] = 'https://gridgroup.cc/';
+$env_s['serv']['grid_site'] = 'https://grid_site';
 
 
 //define('ENV_S', $env_s);
